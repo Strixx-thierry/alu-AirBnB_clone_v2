03f048efead2d88a308712c562359488b8690e99
gusenga thierry <180168889+Strixx-thierry@users.noreply.github.com>
2025-05-25 13:11:28 +0200

    ok

--- models/user.py
+++ models/user.py
@@ -25,7 +25,6 @@ class User(BaseModel, Base):
                           backref="user")
     reviews = relationship("Review", cascade='all, delete, delete-orphan',
                            backref="user")
-    
     def __init__(self, *args, **kwargs):
         """Initialize User instance with default empty string attributes"""
         super().__init__(*args, **kwargs)
